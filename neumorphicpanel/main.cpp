9f803abe9f27be890c1f85c44b1afb193f8a8b61
t-tang-rfc <156632897+t-tang-rfc@users.noreply.github.com>
2025-06-25 22:29:52 +0900

    Slim the repo.

--- neumorphicpanel/main.cpp
+++ neumorphicpanel/main.cpp
@@ -1,6 +1,5 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-// #include "./shared/shared.h"
 
 #include <QGuiApplication>
 #include <QQuickView>
@@ -9,17 +8,17 @@
 
 int main(int argc, char* argv[])
 {
-    QGuiApplication app(argc,argv);
+	QGuiApplication app(argc,argv);
 
-    QQuickView view;
-    view.connect(view.engine(), &QQmlEngine::quit, &app, &QCoreApplication::quit);
-    new QQmlFileSelector(view.engine(), &view);
-    view.setSource(QUrl("qrc:/qt/qml/neumorphicpanel/qml/main.qml"));
-    if (view.status() == QQuickView::Error) {
+	QQuickView view;
+	view.connect(view.engine(), &QQmlEngine::quit, &app, &QCoreApplication::quit);
+	new QQmlFileSelector(view.engine(), &view);
+	view.setSource(QUrl("qrc:/qt/qml/neumorphicpanel/qml/main.qml"));
+	if (view.status() == QQuickView::Error) {
 		qWarning() << "Error loading QML file:" << view.errors();
-        return -1;
+		return -1;
 	}
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.show();
-    return app.exec();
+	view.setResizeMode(QQuickView::SizeRootObjectToView);
+	view.show();
+	return app.exec();
 }
